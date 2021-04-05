7fa619e7ca180d2a2c6594396a66e2c4b7e75b3a
liuchaodada <1677004254@qq.com>
2021-04-05 12:26:30 +0800

    修改

--- pytest_practice/calculator/test_cal.py
+++ pytest_practice/calculator/test_cal.py
@@ -17,42 +17,32 @@ class TestCalculator:
     def teardown_class(self):
         print("结束测试")
 
-    # 测试加法
-    @pytest.mark.parametrize(
-        "a, b, expect", get_datas()["add"]
-    )
-    def test_add(self, a, b, expect):
-        result = self.c.add(a, b)
-        if (isinstance(result,float)):
-            result = round(result, 2)
-        assert result == expect
+    @pytest.mark.parametrize("a, b, expect", datas['add_int']['datas'], ids=datas['add_int']['ids'])
+    def test_add_int(self, a, b, expect):
+        assert self.cal.add(a, b) == expect
 
-    # 测试减法
-    @pytest.mark.parametrize(
-        "a, b, expect", get_datas()["sub"]
-    )
-    def test_sub(self, a, b, expect):
-        result = self.c.sub(a, b)
-        if (isinstance(result, float)):
-            result = round(result, 2)
-        assert result == expect
+    @pytest.mark.parametrize("a, b, expect", datas['add_float']['datas'], ids=datas['add_float']['ids'])
+    def test_add_float(self, a, b, expect):
+        assert round(self.cal.add(a, b), 2) == expect
 
-    # 测试乘法
-    @pytest.mark.parametrize(
-        "a, b, expect", get_datas()["mul"]
-    )
-    def test_mul(self, a, b, expect):
-        result = self.c.mul(a, b)
-        if (isinstance(result, float)):
-            result = round(result, 2)
-        assert result == expect
+    @pytest.mark.parametrize("a, b, expect", datas['add_minus']['datas'], ids=datas['add_minus']['ids'])
+    def test_add_minus(self, a, b, expect):
+        assert self.cal.add(a, b) == expect
 
-    # 测试除法
-    @pytest.mark.parametrize(
-        "a, b, expect", get_datas()["div"]
-    )
-    def test_div(self, a, b, expect):
-        result = self.c.div(a, b)
-        if (isinstance(result, float)):
-            result = round(result, 2)
-        assert result == expect
+    @pytest.mark.parametrize("a, b, expect", datas['div_error']['datas'], ids=datas['div_error']['ids'])
+    def test_div_error(self, a, b, expect): # 可预期的错误
+        with pytest.raises(ZeroDivisionError):
+                assert self.cal.div(a, b) == expect
+
+    @pytest.mark.parametrize("a, b, expect", datas['div_int']['datas'], ids=datas['div_int']['ids'])
+    def test_div_error(self, a, b, expect):
+        assert self.cal.div(a, b) == expect
+
+    # 浮点数取精度进行计算测试
+    @pytest.mark.parametrize("a, b, expect", datas['div_float']['datas'], ids=datas['div_float']['ids'])
+    def test_div_float(self, a, b, expect):
+        assert round(self.cal.div(a, b), 2) == expect
+
+    @pytest.mark.parametrize("a, b, expect", datas['div_minus']['datas'], ids=datas['div_minus']['ids'])
+    def test_div_minus(self, a, b, expect):
+        assert self.cal.div(a, b) == expect
